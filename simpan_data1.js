9202e25b7512b95b52413b01c3844a76bd3352be
januar7 <91745350+januar7@users.noreply.github.com>
2024-07-17 20:09:05 +0700

    hope this work :)

--- simpan_data1.js
+++ simpan_data1.js
@@ -1,22 +1,25 @@
-const script_URL = 'https://script.google.com/macros/s/AKfycbzIJ_iNqhA--z6RDyj04MM57U7TiczsdI6prbutiayDHCQ2W1re9x6TJB9vnOCMe1sI/exec'
-const form = document.forms['final_response']
+//const script_URL = 'https://script.google.com/macros/s/AKfycbzIJ_iNqhA--z6RDyj04MM57U7TiczsdI6prbutiayDHCQ2W1re9x6TJB9vnOCMe1sI/exec'
+// const form = document.forms['final_response']
 
-//function submitform() {
-//    const name = document.getElementById('name').value;
-//  const anggota = document.getElementById('anggota').value.trim().split('\n').map(line => line.trim()).filter(line => line !== '');
 
-/*if (anggota.length === 0) {
-    alert('Anggotanya siapa? Masa engga ada anggota sih :v');
-    return;
-} */
+function submitform() {
+    const name = document.getElementById('name').value;
+    const anggota = document.getElementById('anggota').value.trim().split('\n').map(line => line.trim()).filter(line => line !== '');
+
+    if (anggota.length === 0) {
+        alert('Anggotanya siapa? Masa engga ada anggota sih :v');
+        return;
+    }
+}
+const form = document.getElementById('response_1');
 form.addEventListener('submit', e => {
-    e.preventDefault()
+    e.preventDefault();
+    const script_URL = e.target.action;
 
     fetch(script_URL, {
             method: 'POST',
             body: new FormData(form)
         })
         .then(response => alert('Data sukses diinput'))
-        .then(response => console.log('Data sukses diinput', response))
-        .catch(error => console.error('Error', error.message))
+        .catch(error => console.error('Error!!', error.message))
 });
